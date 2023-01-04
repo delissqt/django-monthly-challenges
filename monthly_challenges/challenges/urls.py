c5191a4c57604bd0e28cfc0a9e5b3d6bf1fb38aa
delia gt <delissqt@gmail.com>
2023-01-04 15:57:20 -0600

    Include Partial Template Snippets
    Algo Usign WITH  in Tag Snippet and pass Parameter

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # string describe URL we wanna supoort // pointer at the view function that should be executed // 
-    path("", views.index), # /challenges/
+    path("", views.index, name="index"), # /challenges/
     path("<int:month>", views.monthly_challenges_by_number), # challenges/1
     path("<str:month>", views.monthly_challenge, name="month-challenge"), #challenges/january
 ]
